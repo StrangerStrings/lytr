b183dff4bd1dd5da94153d99fdaee5999ddb8417
Ben Clayton-Green <ben-clayton-green@Bens-MacBook-Air.local>
2019-04-17 19:05:19 +0100

    finished product

---
+++ src/theGoods.js
@@ -0,0 +1,91 @@
+import React from 'react'
+
+
+const App = (props) => (
+    <div className='app'>
+
+
+
+        <div className='room candle-room'>
+
+            <div className='candle-light-2' ></div>
+            <div className='candle-light-1' ></div>
+
+            <img src="./images/candle1.png"/>
+
+            <div className='darkness' ></div>
+
+        </div>
+
+
+
+        <div className='room tube-room'>
+         
+            <div className='tube-light-2' ></div>
+            <div className='tube-light-1' ></div>
+
+            <img src="./images/tube3.png" />
+
+            <div className='darkness ' ></div>
+
+        </div>
+
+
+        <div className='room fire-room'>
+
+            <div className='fire-ember ember-1-1' ></div>
+            <div className='fire-ember ember-1-2' ></div>
+            <div className='fire-ember ember-1-3' ></div>
+            <div className='fire-ember ember-2-1' ></div>
+            <div className='fire-ember ember-2-2' ></div>
+            <div className='fire-ember ember-2-3' ></div>
+            <div className='fire-ember ember-2-4' ></div>
+            <div className='fire-ember ember-3-1 ember-3' ></div>
+            <div className='fire-ember ember-3-2 ember-3' ></div>
+            <div className='fire-ember ember-3-3 ember-3' ></div>
+            <div className='fire-ember ember-4' ></div>
+
+            <div className='fire-light-2' ></div>
+            <div className='fire-light-1' ></div>
+
+            <img src="./images/fire1.png" />
+
+            <div className='darkness' ></div>
+
+
+
+
+        </div>
+
+
+        <div className='room bum-room'>
+
+            <div className='bum-light-1' ></div>
+            <div className='bum-light-2' ></div>
+            <div className='bum-light-3' ></div>
+            <div className='bum-light-4' ></div>
+
+            <img src="./images/bum6.png" />
+
+            <div className='darkness ' ></div>
+
+
+
+
+        </div>
+
+
+
+        <div className='face'>
+            <div className='eye eye-1' ><div className='pupil' ></div> </div>
+            <div className='eye eye-2' ><div className='pupil' ></div> </div>
+            <div className='mouth' ></div>
+        </div>
+
+
+    </div>
+)
+
+
+
+export default App
